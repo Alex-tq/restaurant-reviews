e9f61e61d91bb357b0e6d2a87bd8d817134a4be0
unknown <alextq3036@gmail.com>
2021-11-11 21:39:26 -0500

    Created a components folder with 4 components. created the navbar in the App.js and added routing with React router

---
+++ frontend/src/components/add-review.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddReview(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddReview
---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function RestaurantsList(props) {
+    console.log('list')
+    return (
+        <div>
+           <h1>restaurants</h1>
+           <h1>restaurants</h1>
+           <h1>restaurants</h1>
+           <h1>restaurants</h1>
+        </div>
+    )
+}
+
+export default RestaurantsList
