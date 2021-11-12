5a1a8a111a8b899cd10f4c9137c1933add8596de
unknown <alextq3036@gmail.com>
2021-11-12 04:06:02 -0500

    Completed the restauranList component and render the restaurants from the database

---
+++ frontend/src/services/restaurant.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+let http = axios.create({
+    baseURL: "http://localhost:5000/api/v1/restaurants",
+    headers: {
+        "Content-type": "application/json"
+    }
+})
+
+class RestaurantDataService {
+  getAll(page = 0) {
+    return http.get(`?page=${page}`);
+  }
+
+  get(id) {
+    return http.get(`/?id=${id}`);
+  }
+
+  find(query, by = "name", page = 0) {
+    return http.get(`?${by}=${query}&page=${page}`);
+  } 
+
+  createReview(data) {
+    return http.post("/review", data);
+  }
+
+  updateReview(data) {
+    return http.put("/review", data);
+  }
+
+  deleteReview(id, userId) {
+    return http.delete(`/review?id=${id}`, {data:{user_id: userId}});
+  }
+
+  getCuisines(id) {
+    return http.get(`/cuisines`);
+  }
+
+}
+
+export default new RestaurantDataService();
